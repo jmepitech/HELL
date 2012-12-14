c2b839cbe004e79f7d3087c5c6e81d959f77324a
carls gipson <gipson_c@pc-gipson-c.(none)>
2012-12-14 23:40:30 +0100

    debut etape2

---
+++ etape2/CMakeFiles/test_function.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/./test_function.dir/main.cpp.o"
+  "./test_function.pdb"
+  "./test_function"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/./test_function.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
