0e9467b5c42249a23f39bf8c925b529132169b70
carls gipson <gipson_c@pc-gipson-c.(none)>
2012-12-15 14:26:42 +0100

    balance l etape 2 bitches

---
+++ purgatory/etape2/Machine.hpp
@@ -0,0 +1,29 @@
+#ifndef __MACHINE_HPP__
+#define __MACHINE_HPP__
+
+#include <iostream>
+
+enum eState
+{
+	S0,
+	S1,
+	S2,
+	S3,
+	S4,
+	S5,
+	S6,
+	S7,
+	STATE_ERROR
+};
+
+enum eAction
+{
+	MA,
+	HR,
+	ACTION_ERROR     
+};
+
+extern eState 		gStateTable[8][8];
+extern eAction		gActionTable[8][8];
+
+#endif
